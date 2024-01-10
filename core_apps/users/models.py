725128366ff7cea8101b1fe2c8ee61c72b3db9bd
mohamedMQ <maqbour.moh@gmail.com>
2024-01-10 10:34:00 +0000

    Add username regex validator to User model

--- core_apps/users/models.py
+++ core_apps/users/models.py
@@ -1,8 +1,18 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 from core_apps.users.managers import UserManager
+
+
+class UsernameValidator(RegexValidator):
+    regex = r"^[a-zA-Z0-9_.-]+$"
+    message = _(
+        "Enter a valid username. This value may contain only letters, "
+        "numbers, and ./_ characters."
+    )
+    flags = 0
 
 
 class User(AbstractUser):
@@ -10,11 +20,19 @@ class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     first_name = models.CharField(verbose_name=_("First Name"), max_length=60)
     last_name = models.CharField(verbose_name=_("Last Name"), max_length=60)
-    email = models.EmailField(verbose_name=_("Email Address"), unique=True, db_index=True)
-    username = models.CharField(verbose_name=_("Username"), max_length=60, unique=True, db_index=True)
+    email = models.EmailField(
+        verbose_name=_("Email Address"), unique=True, db_index=True
+    )
+    username = models.CharField(
+        verbose_name=_("Username"),
+        max_length=60,
+        unique=True,
+        validators=[UsernameValidator()],
+    )
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
+
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
 
     objects = UserManager()
@@ -26,4 +44,5 @@ class User(AbstractUser):
 
     @property
     def get_full_name(self) -> str:
-        return f"{self.first_name} {self.last_name}".strip()
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip()
