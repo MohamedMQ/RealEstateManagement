6292b9764d6ef73594165520ce467c390befcc44
mohamedMQ <maqbour.moh@gmail.com>
2024-01-23 09:12:00 +0000

    Register all app routes and add drf-yasg API docs

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Alpha Apartments API",
+        default_version="v1",
+        description="API endpoints for the Alpha Apartments Application",
+        contact=openapi.Contact(email="maqbour.moh@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("supersecret/", admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/users/", include("core_apps.users.urls")),
+    path("api/v1/profiles/", include("core_apps.profiles.urls")),
+    path("api/v1/apartments/", include("core_apps.apartments.urls")),
+    path("api/v1/issues/", include("core_apps.issues.urls")),
+    path("api/v1/reports/", include("core_apps.reports.urls")),
+    path("api/v1/ratings/", include("core_apps.ratings.urls")),
+    path("api/v1/posts/", include("core_apps.posts.urls")),
 ]
 
 admin.site.site_header = "Alpha Apartments Admin"
