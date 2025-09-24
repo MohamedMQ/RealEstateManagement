935120d80823cce58a924c10a28b779e5426a73c
mohamedMQ <maqbour.moh@gmail.com>
2025-09-24 17:58:16 +0100

    feat: users can report other users for misconduct

--- config/settings/base.py
+++ config/settings/base.py
@@ -55,6 +55,7 @@ LOCAL_APPS = [
     "core_apps.ratings",
     "core_apps.apartments",
     "core_apps.issues",
+    "core_apps.reports",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
