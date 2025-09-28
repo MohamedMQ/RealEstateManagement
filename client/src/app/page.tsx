f6895eba6ea2c5a6a4cf3a118b546da6e9c40dde
mohamedMQ <maqbour.moh@gmail.com>
2025-09-28 12:59:01 +0100

    chore: setup custom fonts, eslint and prettier

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-6xl">Home Page!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="font-robotoSlab text-6xl">Home Page!</h1>
+			<p className="font-openSans text-3xl">
+				Welcome to our Next.js application with Tailwind CSS and custom fonts!
+			</p>
+		</div>
+	);
 }
