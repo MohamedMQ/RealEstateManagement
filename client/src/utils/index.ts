294279a01eeceeaf49f5a73471e50ef6184ec10a
mohamedMQ <maqbour.moh@gmail.com>
2025-09-30 12:47:58 +0100

    feat: show user details on profile page

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -4,3 +4,10 @@ export { default as extractErrorMessage } from "./extractErrorMessage";
 export { default as PersistAuth } from "./PersistAuth";
 
 export const UseGoogle = () => InitiateSocialAuth("google-oauth2", "google");
+
+export { formatDate } from "./formatDate";
+export { capitalizeFirstLetter } from "./capitalizeFirstLetter";
+
+export { getRepliesText } from "./getRepliesText";
+export { getViewText } from "./getViewText";
+export { sortByDateDescending } from "./sortByDate";
